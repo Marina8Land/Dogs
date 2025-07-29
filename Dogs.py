c79db3c6f4ea5c1d212e265c902a9e5b9de423ef
Marina <itmarinaa257@gmal.com>
2025-07-29 13:54:58 +0300

    Создана функция загрузки изображения

--- Dogs.py
+++ Dogs.py
@@ -8,10 +8,19 @@ from io import BytesIO
 
 
 def show_image():
+    """Выводит изображение"""
     image_url = get_dog_image()
     if image_url:
         try:
             response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            img.data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img.thumbnail((300, 300))
+            label.config(image=img)
+            label.image = img
+        except Exception as e:
+            messagebox.showerror('Ошибка', f'Возникла ошибка {e}')
 
 
 window = Tk()
